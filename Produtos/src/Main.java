42a06ea6bb4729b5130816f9636c45f384400a39
ALEXANDRE GABRIEL BONA <alexandre.bona3@alunos.sc.senac.br>
2025-04-14 19:27:45 -0300

    Adicionado scanner,  opcoes para o usuario e variavel inicial para guardar a escolha

--- Produtos/src/Main.java
+++ Produtos/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        List<String> listaProdutos = new ArrayList<>();
+
+        System.out.println("======== CADASTRO DE PRODUTOS ========");
+        System.out.println("1 - Cadastrar um Produto");
+        System.out.println("2 - Listar Produtos");
+        System.out.println("3 - Remover Produto");
+        System.out.println("4 - Alterar Produto");
+        System.out.println("0 - Sair");
+        System.out.print("Escolha uma opção: ");
+        int escolha = sc.nextInt();
+
+
     }
 }
